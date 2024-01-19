d31d1bcb90707314d2932688998e8dcaf3e86d79
ArtashesSoghomonyan <156454394+ArtashesSoghomonyan@users.noreply.github.com>
2024-01-19 22:47:35 +0400

    added generatePassword function

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 func main() {
-	fmt.Println("Password generator!")
+	fmt.Println(generatePassword())
 }
 
+func generatePassword() string {
+	var passwordLength int = 6
+	var result string = ""
+	var characters string = alphabet
+
+	for i := 1; i <= passwordLength; i++ {
+		result += string(characters[rand.Intn(len(characters))])
+	}
+
+	return result
+}
